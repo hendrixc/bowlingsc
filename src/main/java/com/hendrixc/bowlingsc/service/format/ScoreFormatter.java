a2b378411d6e0494aafa8151da8999065452113f
Hendrix <henry.coralc@gmail.com>
2020-05-24 22:05:18 -0500

    Fixed javadoc.

--- src/main/java/com/hendrixc/bowlingsc/service/format/ScoreFormatter.java
+++ src/main/java/com/hendrixc/bowlingsc/service/format/ScoreFormatter.java
@@ -8,17 +8,19 @@ package com.hendrixc.bowlingsc.service.format;
 
 import com.hendrixc.bowlingsc.model.Player;
 
-
 /**
- *
+ * Define the methods to be implemented for a formatter implementation.
+ * A formatter implementation must generate a text from player info.
+ * 
  * @author Henry Coral
  */
 public interface ScoreFormatter {
     
     /**
+     * Produce a string with the information of a player (final score, frames) ready to be printed.
      * 
-     * @param player
-     * @return 
+     * @param player Player object with final score calculated and all their frames. 
+     * @return text with the format and data to be showed.
      */
     String format(Player player);
 }
